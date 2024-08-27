1678f77aeff3e3e4ca1ac12c5c06738cc053826e
Swalih60 <swalihzamnoon60@gmail.com>
2024-08-27 22:32:35 +0530

    feat: Home screen set

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,1 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:musify/screens/first_screen.dart';
+import 'package:musify/screens/library_screen.dart';
+import 'package:musify/screens/upload_screen.dart';
 
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({
+    super.key,
+  });
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> selectedPage = [
+      const FirstScreen(),
+      const UploadScreen(),
+      const LikedSongsScreen()
+    ];
+
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.upload),
+            label: 'Upload',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.library_music),
+            label: 'Liked songs',
+          ),
+        ],
+        selectedItemColor: Colors.purple,
+        currentIndex: _selectedIndex,
+        onTap: (value) {
+          setState(() {
+            _selectedIndex = value;
+          });
+        },
+      ),
+      body: selectedPage[_selectedIndex],
+    );
+  }
+}
